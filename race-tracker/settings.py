c6393892dd92f879678db14d50e693b2ee0f76ac
Damien Plenard <damien@plenard.me>
2015-08-27 19:22:32 +0200

    add settings file

---
+++ race-tracker/settings.py
@@ -0,0 +1,16 @@
+"""
+Settings file for the race-tracker IRC bot.
+"""
+import os
+
+
+IRC_SETTINGS = {
+    'server': 'irc2.speedrunslive.com',
+    'port': 6667,
+    'nickname': 'rtbot',
+    'channel': '#speedrunslive',
+}
+
+API_SETTINGS = {
+    'url': 'http://localhost:8000'
+}
