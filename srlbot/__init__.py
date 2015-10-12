df040289c3b3b9823a524031b25f1735364ba057
Damien Plenard <damien@plenard.me>
2015-10-12 23:28:05 +0200

    support for command line argument

--- srlbot/__init__.py
+++ srlbot/__init__.py
@@ -2,15 +2,29 @@
 Provide main entry function to run the bot.
 """
 import logging
+import argparse
 
 from .srlbot import SRLBot
 from .commands import HTTPSRLCommands
+
+
+def handle_argument():
+    """
+    Handle argument given by command line.
+    """
+    parser = argparse.ArgumentParser(
+        description='Monitor & handle SRL command from an IRC channel.'
+    )
+    parser.add_argument('-c', '--config',  metavar='filename', type=str,
+                        nargs=1, help='set configuration file.')
+    return parser.parse_args()
 
 
 def main():
     """
     Create and run the bot.
     """
+    options = handle_argument()
     bot = SRLBot(channel="#race-tracker")
     command = HTTPSRLCommands(
         bot, ['dam0un'], {'startrace': 'http://localhost'}
